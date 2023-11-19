d2ea2b23a0df0a0c5f82997cb91658913b4156bb
GElshan <elshankhanlari@gmail.com>
2023-11-19 15:46:57 +0100

    git repo refactor

---
+++ gateway/app.py
@@ -0,0 +1,68 @@
+import requests
+import os
+
+from flask import Flask, render_template, request, make_response
+from requests.exceptions import ConnectionError, HTTPError
+from urls import *
+
+ALLOWED_MATH_OPS = ["add", "sub", "mul", "div", "mod"]
+ALLOWED_STR_OPS = ["lower", "upper", "concat", "editdistance"]
+ALLOWED_LOG_OPS = ["getLog"]
+LOG_URL = os.environ["LOG_URL"]
+
+app = Flask(__name__, instance_relative_config=True)
+
+
+@app.route("/math/<op>")
+def math(op):
+    a = request.args.get("a", type=float)
+    b = request.args.get("b", type=float)
+    if op not in ALLOWED_MATH_OPS:
+        return make_response("Invalid operation\n", 404)
+    try:
+        x = requests.get(MATH_URL + f"/{op}?a={a}&b={b}")
+        x.raise_for_status()
+        return x.json()
+    except ConnectionError:
+        return make_response("Math service is down\n", 404)
+    except HTTPError:
+        return make_response("Invalid input\n", 400)
+
+
+@app.route("/str/<op>")
+def string(op):
+    a = request.args.get("a", type=str)
+    b = request.args.get("b", type=str)
+    if op not in ALLOWED_STR_OPS:
+        return make_response("Invalid operation\n", 404)
+    try:
+        if op == "lower" or op == "upper":
+            x = requests.get(STRING_URL + f"/{op}?a={a}")
+        else:
+            x = requests.get(STRING_URL + f"/{op}?a={a}&b={b}")
+        x.raise_for_status()
+        return x.json()
+    except ConnectionError:
+        return make_response("Math service is down\n", 404)
+    except HTTPError:
+        return make_response("Invalid input\n", 400)
+
+
+@app.route("/log/<op>")
+def log(op):
+    srv = request.args.get("srv", type=str)
+    if op not in ALLOWED_LOG_OPS:
+        return make_response("Invalid operation\n", 404)
+    try:
+        x = requests.get(LOG_URL + f"/{op}/{srv}")
+        print(x)
+        x.raise_for_status()
+        return x.json()
+    except ConnectionError:
+        return make_response("LOG service is down\n", 404)
+    except HTTPError:
+        return make_response("Invalid input\n", 400)
+
+
+def create_app():
+    return app
---
+++ gateway/urls.py
@@ -0,0 +1,3 @@
+GATEWAY_URL = 'http://GATEWAY_URL:5000' #also add gateway service port
+MATH_URL = 'http://MATH_URL:5000' #also add math service port
+STRING_URL = 'http://STRING_URL:5000' #also add string service port
